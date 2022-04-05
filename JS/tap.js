a90fc49a01bd58db965873f1fd91a6df4caadaf4
kjh <arisol8246@naver.com>
2022-04-05 09:45:11 +0900

    0404

---
+++ JS/tap.js
@@ -0,0 +1,34 @@
+// for(let i=0;i<$('.tab-button').length;i++){
+
+//     $('.tab-button').eq(i).on('click', function(){
+//         탭열기(i)
+//     })
+// }
+
+$('.list').click(function(e){
+    console.log(e.target.dataset.id)
+        탭열기(e.target.dataset.id)
+    
+    
+})
+
+function 탭열기(숫자){
+    $('.tab-button').removeClass('orange');
+        $('.tab-content').removeClass('show');
+        $('.tab-button').eq(숫자).addClass('orange');
+        $('.tab-content').eq(숫자).addClass('show');
+}
+
+
+// $('.tab-button').eq(1).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-button').eq(1).addClass('orange');
+//     $('.tab-content').eq(1).addClass('show');
+// })
+// $('.tab-button').eq(2).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-button').eq(2).addClass('orange');
+//     $('.tab-content').eq(2).addClass('show');
+// })
